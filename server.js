bd17281d1d7fb3222b35c6adacfaefec46ac897c
muscleheadDeveloper <njokua04@gmail.com>
2022-10-17 10:27:44 +0100

    server.js fixed

--- server.js
+++ server.js
@@ -23,9 +23,9 @@ app.get("/api/config/paypal", (req, res) => {
 });
 
 //MIDDLEWARE STATIC && FRONTEND FILE
-app.use(express.static(path.resolve(__dirname, "/frontend/build")));
+app.use(express.static(path.join(__dirname, "frontend", "build")));
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "/frontend/build", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./frontend/build/", "index.html"));
 });
 
 //ERROR HANDLERS
